2c7046babebb8238d9d2c6cc85707dca1a217d1a
skyfoojs <skyfoojs@gmail.com>
2024-12-10 15:39:57 +0800

    Add component and waiter directory

---
+++ laravel/resources/views/waiter/login.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+<div class="flex flex-col justify-center items-center">
+    <!-- An unexamined life is not worth living. - Socrates -->
+    <div class="flex mt-28 bg-neutral-300/[.6] rounded-xl">
+        <img class="w-72" src="{{ asset('images/' . 'TastyByte-Banner.png') }}" alt="">
+    </div>
+
+    <div class="mt-10 flex flex-col justify-center items-center">
+        <h1 class="">VTEN Kopitiam</h1>
+        <h1 class="">Login</h1>
+    </div>
+</div>
+</x-layout>
