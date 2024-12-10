b7cbe4adeb855e08fed141fdc987d0018cc80502
skyfoojs <skyfoojs@gmail.com>
2024-12-10 15:40:23 +0800

    Add login route

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Get the view from Login Controller, and call the login() function.
+Route::get('/login', [LoginController::class, 'index'])->name('login');
