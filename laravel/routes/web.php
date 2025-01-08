f8f3b0ec86b6b6b2eaae9bbb26d30c17b72c9e30
skyfoojs <skyfoojs@gmail.com>
2025-01-08 11:07:16 +0800

    Add routes for menu page

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Get the view from Login Controller, and call the login() function.
+// Get the view from Login Controller.
 Route::get('/login', [LoginController::class, 'index'])->name('login');
+
+// Get the view from Product Controller.
+Route::get('/menu', [ProductController::class, 'index'])->name('menu');
