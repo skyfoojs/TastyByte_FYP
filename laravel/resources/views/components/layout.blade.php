7425b157c4b681a0d065209ebc391132aa43310b
skyfoojs <skyfoojs@gmail.com>
2025-01-09 11:43:16 +0800

    Remove unuse code

--- laravel/resources/views/components/layout.blade.php
+++ laravel/resources/views/components/layout.blade.php
@@ -5,10 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
