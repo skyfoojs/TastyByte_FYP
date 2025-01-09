6b8318192c2497d5e34fe68fcd687d3e8bd9adec
skyfoojs <skyfoojs@gmail.com>
2025-01-09 11:47:52 +0800

    Modify /order route and create /menu and /table route

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\OrdersController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,4 +15,10 @@ Route::get('/', function () {
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 
 // Get the view from Product Controller.
-Route::get('/menu', [ProductController::class, 'index'])->name('menu');
+Route::get('/order', [ProductController::class, 'index'])->name('order');
+
+// Get the view from Menu Controller.
+Route::get('/menu', [MenuController::class, 'mainMenu'])->name('mainMenu');
+
+// Get the view from Table Controller.
+Route::get('/table', [OrdersController::class, 'index'])->name('table');
