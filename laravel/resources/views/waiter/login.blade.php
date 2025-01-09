8afb071b5452fbc7bcee1e0083af2b4a68bcfa01
skyfoojs <skyfoojs@gmail.com>
2025-01-09 11:42:10 +0800

    Add min height

--- laravel/resources/views/waiter/login.blade.php
+++ laravel/resources/views/waiter/login.blade.php
@@ -1,15 +1,15 @@
 <x-layout>
-<div class="flex flex-col h-screen bg-[#F5F5F5] items-center">
-    <div class="flex bg-white w-full justify-center items-center shadow-[0px_2px_10px_0px_rgba(0,0,0,0.25)]">
-        <img class="w-52 pt-3 pb-2" src="{{ asset('images/' . 'TastyByte-Banner.png') }}" alt="">
-    </div>
+<div class="flex bg-white w-full justify-center items-center shadow-[0px_2px_10px_0px_rgba(0,0,0,0.25)]">
+    <img class="w-52 pt-3 pb-2" src="{{ asset('images/' . 'TastyByte-Banner.png') }}" alt="">
+</div>
 
+<div class="flex flex-col xl:flex-row xl:justify-evenly bg-[#F5F5F5] items-center min-h-screen">
     <div class="mt-1 flex flex-col justify-center items-center">
         <h1 class="font-semibold mt-5">Restaurant's Information</h1>
 
         <div class="flex py-4 px-8 gap-4 bg-white shadow-[2px_2px_10px_0px_rgba(0,0,0,0.25)] mt-4 mx-4 rounded-xl">
             <img class="w-24 rounded-2xl" src="{{ asset('images/' . 'VTEN-Kopitiam-Logo.jpg') }}" alt="Logo.jpg">
-            <p class="text-xs text-[#7A7A7A] leading-5 text-center mt-2">VTEN Kopitiam, Level 12, Menara PJH, Jalan Tun Abdul Razak, Precinct 2 Putrajaya, 62100, Selangor Malaysia</p>
+            <p class="text-xs text-[#7A7A7A] leading-5 text-center xl:w-64 mt-2">VTEN Kopitiam, Level 12, Menara PJH, Jalan Tun Abdul Razak, Precinct 2 Putrajaya, 62100, Selangor Malaysia</p>
         </div>
     </div>
 
