6a5c02dc5c2abab004399ed7493dc8e5ae31f30f
skyfoojs <skyfoojs@gmail.com>
2025-01-09 11:33:05 +0800

    Create table page

---
+++ laravel/resources/views/waiter/table.blade.php
@@ -0,0 +1,18 @@
+<x-layout>
+    <x-navbar>
+        <div class="flex flex-col">
+            <div class="flex w-full mt-4 items-center justify-center relative">
+                <i class='bx bx-chevron-left bx-md absolute left-3'></i>
+                <div class="text-lg font-semibold">Table Number</div>
+            </div>
+
+            <div>
+                <p>Please Enter the Table Number</p>
+            </div>
+
+            <form action="" method="POST">
+                <input type="number">
+            </form>
+        </div>
+    </x-navbar>
+</x-layout>
