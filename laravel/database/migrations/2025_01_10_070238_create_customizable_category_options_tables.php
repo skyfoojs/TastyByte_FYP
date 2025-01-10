b3860162b1dab65071e2a7e2421b8c39274b2edc
skyfoojs <skyfoojs@gmail.com>
2025-01-10 17:21:50 +0800

    Create database for customizable category and customizable options

---
+++ laravel/database/migrations/2025_01_10_070238_create_customizable_category_options_tables.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customizablecategory', function (Blueprint $table) {
+            $table->id('customizeCategoryID');
+            $table->foreignId('productID')->constrained('product', 'productID')->onDelete('cascade');
+            $table->string('name');
+            $table->string('status');
+            $table->string('sort');
+            $table->boolean('singleChoose');
+            $table->timestamps();
+        });
+
+        Schema::create('customizableoptions', function (Blueprint $table) {
+            $table->id('customizeOptionsID');
+            $table->foreignId('customizeCategoryID')->constrained('customizablecategory', 'customizeCategoryID')->onDelete('cascade');
+            $table->string('name');
+            $table->integer('maxAmount');
+            $table->string('status');
+            $table->string('sort');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customizablecategory');
+        Schema::dropIfExists('customizableoptions');
+    }
+};
