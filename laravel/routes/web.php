1411568b282de3f7c8239829a3eab70fca24069d
skyfoojs <skyfoojs@gmail.com>
2025-01-13 21:49:02 +0800

    Add route for /admin/users

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\OrdersController;
 use App\Http\Controllers\ProductController;
@@ -15,6 +16,9 @@ Route::get('/', function () {
 // Get the view from Login Controller.
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 
+// Get the login credentials from the view.
+Route::post('/login', [LoginController::class, 'loginPost'])->name('login.post');
+
 // Get the view from Product Controller.
 Route::get('/order', [ProductController::class, 'index'])->name('order');
 
@@ -24,5 +28,14 @@ Route::get('/menu', [MenuController::class, 'mainMenu'])->name('mainMenu');
 // Get the view from Table Controller.
 Route::get('/table', [OrdersController::class, 'index'])->name('table');
 
+// Get the view from Product Details Controller.
+Route::get('/product-details/{id}', [ProductController::class, 'edit'])->name('product-details');
 
-Route::get('/product-details/{id}', [ProductDetailsController::class, 'edit']);
+// Get the User Page view from Admin Controller.
+Route::get('/admin/users', [AdminController::class, 'users'])->name('admin-users');
+
+// Get the Create Users data from the View.
+Route::post('/admin/users', [AdminController::class, 'addUserPost'])->name('addUser.post');
+
+// Get the Edit Users Data from the View.
+Route::put('/admin/users', [AdminController::class, 'editUserPost'])->name('editUser.post');
