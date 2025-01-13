9b3c28ffc39876642e004c5ae43756f265f9572c
skyfoojs <skyfoojs@gmail.com>
2025-01-13 22:59:37 +0800

    Pass table number default value to component

--- laravel/resources/views/waiter/table.blade.php
+++ laravel/resources/views/waiter/table.blade.php
@@ -1,13 +1,17 @@
 <x-waiter.layout>
     <x-waiter.navbar>
-        <x-waiter.table-header>
-        <div class="flex flex-col">
-            <div>
-                <p>Please Enter the Table Number</p>
+        <x-waiter.table-header :table="'Number'">
+        <div class="flex flex-col justify-center items-center">
+            <div class="mt-20">
+                <p class="text-xl">Please Enter the Table Number</p>
             </div>
 
-            <form action="" method="POST">
-                <input type="number">
+            <form action="{{ route('storeTable') }}" method="POST">
+                @csrf
+                <input name="table" type="number" id="table" class="w-72 border border-gray-300 rounded-lg py-2 px-3 mt-1 bg-[#DDDDDD]" required>
+                <button type="submit" class="bg-indigo-500 hover:bg-indigo-600 text-white text-sm font-medium py-2 px-4 rounded-lg flex items-center space-x-2">
+                    <span>Confirm</span>
+                </button>
             </form>
         </div>
         </x-waiter.table-header>
