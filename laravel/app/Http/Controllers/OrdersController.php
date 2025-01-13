b10d05d64bfb3fb202b8a6511cb41f566010f918
skyfoojs <skyfoojs@gmail.com>
2025-01-13 22:58:22 +0800

    Create function storeTable() - Make the table number into a session variable to use it globally

--- laravel/app/Http/Controllers/OrdersController.php
+++ laravel/app/Http/Controllers/OrdersController.php
@@ -9,4 +9,16 @@ class OrdersController extends Controller
     public function index() {
         return view('waiter.table');
     }
+
+    public function storeTable(Request $request) {
+        $request->validate([
+            'table' => 'required|integer|min:1|max:20',
+        ]);
+
+        // Store the table number into a session global variable.
+        session(['tableNo' => $request->input('table')]);
+
+        // Redirect to order page.
+        return redirect()->route('order')->with('success', 'Table number stored in session!');
+    }
 }
