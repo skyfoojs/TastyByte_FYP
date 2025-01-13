fc841cc289c8d866527b7e54a119c7635f66de00
skyfoojs <skyfoojs@gmail.com>
2025-01-13 22:57:54 +0800

    Add {{  }} - To pass table value into the component

--- laravel/resources/views/components/waiter/table-header.blade.php
+++ laravel/resources/views/components/waiter/table-header.blade.php
@@ -2,7 +2,7 @@
     <i class='bx bx-chevron-left bx-md absolute left-3 cursor-pointer' onclick="window.history.back()"></i>
 
     <!-- TODO: Implement dynamic table number. -->
-    <div class="text-lg font-semibold">Table 2</div>
+    <div class="text-lg font-semibold">Table {{ $table }}</div>
 </div>
 
 <main>
