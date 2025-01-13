ccb8e32739fdb405c2e6b6f0d26c53bdffa95ea7
skyfoojs <skyfoojs@gmail.com>
2025-01-13 22:58:53 +0800

    Add route link for order

--- laravel/resources/views/waiter/menu.blade.php
+++ laravel/resources/views/waiter/menu.blade.php
@@ -5,10 +5,10 @@
         <p class="mt-6 font-semibold">Menu</p>
 
         <div class="grid grid-cols-2 gap-x-6 gap-y-7 mt-5">
-            <div class="bg-[#DDD] px-10 pt-10 pb-7 rounded-lg flex flex-col justify-center items-center">
+            <a href="{{ route('table') }}" class="bg-[#DDD] px-10 pt-10 pb-7 rounded-lg flex flex-col justify-center items-center">
                 <div class="bg-[#F0F0F0] p-10 rounded-lg"></div>
                 <p class="mt-2">Order</p>
-            </div>
+            </a>
 
             <div class="bg-[#DDD] px-10 pt-10 pb-7 rounded-lg flex flex-col justify-center items-center">
                 <div class="bg-[#F0F0F0] p-10 rounded-lg"></div>
