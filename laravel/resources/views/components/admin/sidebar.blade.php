a8428aac2958f6e552d2748489e35926ef85147e
skyfoojs <skyfoojs@gmail.com>
2025-01-13 22:12:24 +0800

    Add change color to current page

--- laravel/resources/views/components/admin/sidebar.blade.php
+++ laravel/resources/views/components/admin/sidebar.blade.php
@@ -1,13 +1,11 @@
+<?php $current_page = basename($_SERVER['PHP_SELF']); ?>
+
 <aside class="w-64 bg-[#E8E8E8] h-full fixed">
     <div class="p-4">
         <a href="../../../views/admin/pages/index.php">
-            <img src="../../public/images/Logo.png" alt="Logo" class="w-48 mx-auto">
+            <img src="{{ asset('images/' . 'TastyByte-Banner.png') }}" alt="Logo" class="w-48 mx-auto">
         </a>
     </div>
-
-    <?php
-    $current_page = basename($_SERVER['PHP_SELF']);
-    ?>
 
     <nav class="px-6 text-gray-700">
         <h2 class="text-gray-500 mt-2 mb-5 px-5 font-medium">Menu</h2>
@@ -15,15 +13,15 @@
             <li>
                 <!--Dashboard-->
                 <a href="../admin/index.php"
-                   class="flex items-center space-x-5 py-3 px-5 <?php echo $current_page == 'index.php' ? 'bg-indigo-500 text-white' : 'hover:bg-gray-100'; ?> rounded-md">
+                   class="flex items-center space-x-5 py-3 px-5 {{ $current_page == 'dashboard' ? 'bg-indigo-500 text-white' : 'hover:bg-gray-100' }} rounded-md">
                     <i class="bx bxs-dashboard text-base"></i>
                     <span class="font-medium">Dashboard</span>
                 </a>
             </li>
             <li>
                 <!--Users-->
-                <a href="../admin/users.php"
-                   class="flex items-center space-x-5 py-3 px-5 <?php echo $current_page == 'users.php' ? 'bg-indigo-500 text-white' : 'hover:bg-gray-100'; ?> rounded-md">
+                <a href="../admin/users"
+                   class="flex items-center space-x-5 py-3 px-5 {{ $current_page == 'users' ? 'bg-indigo-500 text-white' : 'hover:bg-gray-100' }} rounded-md">
                     <i class="bx bx-user text-base"></i>
                     <span class="font-medium">Users</span>
                 </a>
