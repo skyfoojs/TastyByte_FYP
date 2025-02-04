d3634bd3fd0be943aa63d6e44708f535e5e59403
skyfoojs <skyfoojs@gmail.com>
2025-02-04 15:52:03 +0800

    Create model for Vouchers table

---
+++ laravel/app/Models/Vouchers.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vouchers extends Model
+{
+    use HasFactory;
+
+    protected $table = 'vouchers';
+    protected $primaryKey = 'voucherID';
+
+    public $timestamps = true;
+
+    protected $fillable = [
+        'code', 'type', 'singleUse', 'usage', 'value', 'startedOn', 'expiredOn', 'usedCount',
+    ];
+}
