d4941f65a03cccac9d98654aad3907f458e4fab2
skyfoojs <skyfoojs@gmail.com>
2025-02-05 08:52:31 +0800

    Add POST method route for create vouchers

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -61,3 +61,6 @@ Route::put('admin/inventory', [AdminController::class, 'editInventoryPost'])->na
 
 // Get the Voucher Page view from Admin Controller.
 Route::get('admin/vouchers', [AdminController::class, 'vouchers'])->name('admin-vouchers');
+
+// Get the Create Vouchers Data from the View.
+Route::post('admin/vouchers', [AdminController::class, 'addVoucherPost'])->name('addVoucher.post');
