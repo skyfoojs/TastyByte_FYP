7cc6d3d2d858f9577778b37345e8948ca403fe56
skyfoojs <skyfoojs@gmail.com>
2025-02-07 22:32:06 +0800

    Add relationship, mass assigned variable and table

--- laravel/app/Models/OrderItems.php
+++ laravel/app/Models/OrderItems.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class OrderItems extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'orderitems';
+    protected $primaryKey = 'orderItemID';
+
+    public $timestamps = true;
+
+    protected $fillable = [
+        'orderID', 'productID', 'quantity', 'remark'
+    ];
+
+    public function orders()
+    {
+        return $this->belongsTo(Orders::class, 'orderID');
+    }
 }
