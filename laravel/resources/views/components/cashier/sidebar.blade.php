d2809702132c716212c5e4910d93afd142f5219e
i4GS <admin@i4.gs>
2025-02-08 20:45:09 +0800

    Added sidebar as component for cashier page

---
+++ laravel/resources/views/components/cashier/sidebar.blade.php
@@ -0,0 +1,29 @@
+<div class="w-1/4 bg-white p-6 shadow-lg fixed right-0 top-26 h-[calc(100%-6rem)] flex flex-col">
+    <h2 class="font-semibold text-lg">Table 2 Summary</h2>
+
+    <div class="flex-1 overflow-y-auto mt-4">
+    <!--TODO: Order Product -->
+    </div>
+
+    <div class="border-t pt-4">
+
+        <div class="flex justify-between text-gray-600">
+            <span>Subtotal</span>
+            <span>20.00</span>
+        </div>
+        <div class="flex justify-between text-gray-600">
+            <span>Tax 6%</span>
+            <span>1.20</span>
+        </div>
+        <div class="flex justify-between text-gray-600">
+            <span>Service Charge 10%</span>
+            <span>2.00</span>
+        </div>
+        <div class="flex justify-between font-semibold">
+            <span>Total</span>
+            <span>23.20</span>
+        </div>
+        <button class="w-full bg-indigo-400 text-white py-2 mt-4 mb-4 rounded">Place Order</button>
+    </div>
+</div>
+
