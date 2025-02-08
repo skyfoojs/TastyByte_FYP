07ecd8defb02823eb73ddc248e24aacdc6e47c63
skyfoojs <skyfoojs@gmail.com>
2025-02-08 15:31:23 +0800

    Add link for track order button

--- laravel/resources/views/waiter/menu.blade.php
+++ laravel/resources/views/waiter/menu.blade.php
@@ -1,19 +1,23 @@
 <x-waiter.layout>
     <x-waiter.navbar>
 
-    <div class="flex flex-col justify-center items-center">
+    <div class="flex flex-col justify-center items-center px-2">
         <p class="mt-6 font-semibold">Menu</p>
 
-        <div class="grid grid-cols-2 gap-x-6 gap-y-7 mt-5">
+        <div class="grid grid-cols-2 gap-x-4 gap-y-7 mt-5">
             <a href="{{ route('table') }}" class="bg-[#DDD] px-10 pt-10 pb-7 rounded-lg flex flex-col justify-center items-center">
-                <div class="bg-[#F0F0F0] p-10 rounded-lg"></div>
+                <div class="bg-[#F0F0F0] p-2 rounded-lg">
+                    <img class="" src="{{ asset('images/' . 'menu-order.png') }}" alt="">
+                </div>
                 <p class="mt-2">Order</p>
             </a>
 
-            <div class="bg-[#DDD] px-10 pt-10 pb-7 rounded-lg flex flex-col justify-center items-center">
-                <div class="bg-[#F0F0F0] p-10 rounded-lg"></div>
+            <a href="{{ route('trackOrder') }}" class="bg-[#DDD] px-10 pt-10 pb-7 rounded-lg flex flex-col justify-center items-center">
+                <div class="bg-[#F0F0F0] p-2 rounded-lg">
+                    <img class="" src="{{ asset('images/' . 'menu-track-order.png') }}" alt="">
+                </div>
                 <p class="mt-2">Track Order</p>
-            </div>
+            </a>
         </div>
     </div>
     </x-waiter.navbar>
