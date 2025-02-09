5b80b25652f8abc5b6ac97f5bed82cb4d2c6e995
i4GS <admin@i4.gs>
2025-02-09 16:02:18 +0800

    Added key source for cashier to be use in orders controller

--- laravel/resources/views/cashier/table.blade.php
+++ laravel/resources/views/cashier/table.blade.php
@@ -6,12 +6,16 @@
                     <div class="p-6">
                         <h2 class="text-lg font-semibold text-gray-900 mb-6">Enter Table Number</h2>
 
-                        <input type="number" id="table-number"
-                               class="mb-6 w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
-                               placeholder="Please enter a table number">
-                        <div class="flex justify-end space-x-4">
-                            <button class="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">Confirm</button>
-                        </div>
+                        <form action="{{ route('storeTable') }}" method="POST">
+                            @csrf
+                            <input name="table" type="number" id="table"
+                                   class="mb-6 w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
+                                   placeholder="Please enter a table number" required>
+                            <input type="hidden" name="source" value="cashier">
+                            <div class="flex justify-end space-x-4">
+                                <button type="submit" class="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">Confirm</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
