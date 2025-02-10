1f682abe10a14ee5d9292fcc87a1c68691b2ed25
skyfoojs <skyfoojs@gmail.com>
2025-02-10 22:54:15 +0800

    Modify background, text color, hover color, selected color

--- laravel/resources/views/components/admin/sidebar.blade.php
+++ laravel/resources/views/components/admin/sidebar.blade.php
@@ -3,20 +3,20 @@ $current_route = Route::currentRouteName();
 $is_users_page = Str::contains($current_route, 'users'); // Check if the route contains 'users'
 ?>
 
-<aside class="w-64 bg-[#E8E8E8] h-full fixed">
+<aside class="w-64 bg-gray-800 h-full fixed">
     <div class="p-4">
         <a href="{{ url('admin') }}">
             <img src="{{ asset('images/' . 'TastyByte-Banner.png') }}" alt="Logo" class="w-48 mx-auto">
         </a>
     </div>
 
-    <nav class="px-6 text-gray-700">
-        <h2 class="text-gray-500 mt-2 mb-5 px-5 font-medium">Menu</h2>
+    <nav class="px-6 text-gray-300">
+        <h2 class="mt-2 mb-5 px-5 font-medium">Menu</h2>
         <ul class="space-y-4">
             <li>
                 <!-- Dashboard -->
                 <a href=""
-                   class="flex items-center space-x-5 py-3 px-5 {{ $current_route == 'admin.dashboard' ? 'bg-indigo-500 text-white' : 'hover:bg-gray-100' }} rounded-md">
+                   class="hover:text-white flex items-center space-x-5 py-3 px-5 {{ $current_route == 'admin.dashboard' ? 'bg-gray-900 text-white' : 'hover:bg-gray-700' }} rounded-md">
                     <i class="bx bxs-dashboard text-base"></i>
                     <span class="font-medium">Dashboard</span>
                 </a>
@@ -24,7 +24,7 @@ $is_users_page = Str::contains($current_route, 'users'); // Check if the route c
             <li>
                 <!-- Users -->
                 <a href="{{ route('admin-users') }}"
-                   class="flex items-center space-x-5 py-3 px-5 {{ $is_users_page ? 'bg-indigo-500 text-white' : 'hover:bg-gray-100' }} rounded-md">
+                   class="hover:text-white flex items-center space-x-5 py-3 px-5 {{ $is_users_page ? 'bg-gray-900 text-white' : 'hover:bg-gray-700' }} rounded-md">
                     <i class="bi bi-people-fill"></i>
                     <span class="font-medium">Users</span>
                 </a>
@@ -32,7 +32,7 @@ $is_users_page = Str::contains($current_route, 'users'); // Check if the route c
             <li>
                 <!-- Products -->
                 <a href="{{ route('admin-products') }}"
-                   class="flex items-center space-x-5 py-3 px-5 {{ $current_route == 'admin-products' ? 'bg-indigo-500 text-white' : 'hover:bg-gray-100' }} rounded-md">
+                   class="hover:text-white flex items-center space-x-5 py-3 px-5 {{ $current_route == 'admin-products' ? 'bg-gray-900 text-white' : 'hover:bg-gray-700' }} rounded-md">
                     <i class="bx bxs-package text-base"></i>
                     <span class="font-medium">Products</span>
                 </a>
@@ -40,7 +40,7 @@ $is_users_page = Str::contains($current_route, 'users'); // Check if the route c
             <li>
                 <!-- Inventory -->
                 <a href="{{ route('admin-inventory') }}"
-                   class="flex items-center space-x-5 py-3 px-5 {{ $current_route == 'admin-inventory' ? 'bg-indigo-500 text-white' : 'hover:bg-gray-100' }} rounded-md">
+                   class="hover:text-white flex items-center space-x-5 py-3 px-5 {{ $current_route == 'admin-inventory' ? 'bg-gray-900 text-white' : 'hover:bg-gray-700' }} rounded-md">
                     <i class="bi bi-kanban"></i>
                     <span class="font-medium">Inventory</span>
                 </a>
@@ -48,7 +48,7 @@ $is_users_page = Str::contains($current_route, 'users'); // Check if the route c
             <li>
                 <!-- Vouchers -->
                 <a href="{{ route('admin-vouchers') }}"
-                   class="flex items-center space-x-5 py-3 px-5 {{ $current_route == 'admin-vouchers' ? 'bg-indigo-500 text-white' : 'hover:bg-gray-100' }} rounded-md">
+                   class="hover:text-white flex items-center space-x-5 py-3 px-5 {{ $current_route == 'admin-vouchers' ? 'bg-gray-900 text-white' : 'hover:bg-gray-700' }} rounded-md">
                     <i class="bi bi-ticket-detailed-fill"></i>
                     <span class="font-medium">Vouchers</span>
                 </a>
