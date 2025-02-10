e0f26cc59573ae267cff090adbea2dbdfcd659eb
i4GS <admin@i4.gs>
2025-02-10 08:26:06 +0800

    Make added product show dynamically

--- laravel/resources/views/components/cashier/sidebar.blade.php
+++ laravel/resources/views/components/cashier/sidebar.blade.php
@@ -1,9 +1,50 @@
 <div class="w-1/4 bg-white p-6 shadow-lg fixed right-0 top-26 h-[calc(100%-6rem)] flex flex-col">
     <h2 class="font-semibold text-lg">Table {{ session('tableNo') }} Summary</h2>
 
-    <div class="flex-1 overflow-y-auto mt-4">
-        <!-- TODO: Order Product -->
+    <hr class="mt-4">
+
+    <div class="flex-1 overflow-y-auto mt-6">
+        @php
+            //session()->forget('cart');
+        @endphp
+
+        @if (session('cart'))
+            @foreach (session('cart') as $cartItem)
+                <div class="flex gap-x-4 mb-6">
+                    <!-- Image -->
+                    <div class="border w-24 h-24 rounded-lg flex items-center justify-center">
+                        @if (!empty($cartItem['image']))
+                            <img class="w-full h-full rounded-lg object-cover" src="{{ asset($cartItem['image']) }}" alt="Image Not Available">
+                        @else
+                            <p>No Image</p>
+                        @endif
+                    </div>
+
+                    <!-- Name and Price at the top -->
+                    <div class="flex-1 flex flex-col justify-between">
+                        <div>
+                            <p class="text-base font-bold text-zinc-700">{{ $cartItem['name'] }}</p>
+                            <p class="text-sm text-gray-500">{{ 'RM ' . $cartItem['price'] }}</p>
+
+                            <!-- Options -->
+                            @foreach ($cartItem['options'] as $optionName => $optionValues)
+                                <p class="text-xs text-gray-600">{{ $optionName }}:<br> {{ implode(', ', $optionValues) }}</p>
+                            @endforeach
+                        </div>
+                    </div>
+
+                    <!-- Centered Quantity Box -->
+                    <div class="flex items-center justify-center">
+                        <div class="border w-8 h-8 mr-2 rounded-full bg-indigo-500 flex items-center justify-center">
+                            <p class="text-white">{{ $cartItem['quantity'] }}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        @endif
     </div>
+
+    <hr>
 
     <div class="pt-4">
         @php
@@ -34,7 +75,7 @@
         </div>
         <hr class="border-t-4 mt-4 border-dotted border-gray-200">
         <a href="{{ route('orderSummary') }}">
-            <button class="w-full bg-indigo-400 text-white py-2 mt-4 rounded">Place Order</button>
+            <button class="w-full bg-indigo-500 text-white py-2 mt-4 rounded">Place Order</button>
         </a>
     </div>
 </div>
