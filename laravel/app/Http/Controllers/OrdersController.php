87ae3bf4f17aef8911413eea67d5a269c5349dd4
i4GS <admin@i4.gs>
2025-02-17 10:10:54 +0800

    Removed use Illuminate\Support\Facades\Route;

--- laravel/app/Http/Controllers/OrdersController.php
+++ laravel/app/Http/Controllers/OrdersController.php
@@ -8,7 +8,6 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
 
 class OrdersController extends Controller
 {
