769255fd6eef450b6f71c716a69261d61286e65e
skyfoojs <skyfoojs@gmail.com>
2025-02-17 11:28:48 +0800

    Modify orderby - Change to updated_at and descending order

--- laravel/app/Http/Controllers/AdminController.php
+++ laravel/app/Http/Controllers/AdminController.php
@@ -122,7 +122,7 @@ class AdminController extends Controller
             'vouchersCount' => Vouchers::count(),
             'lowStock' => Inventory::where('stockLevel', '<', 100)->get(),
             'upcomingVouchers' => Vouchers::where('startedOn', '>', now())->get(),
-            'payments' => Payment::orderBy('created_at', 'asc')->get(),
+            'payments' => Payment::orderBy('updated_at', 'desc')->get(),
         ]);
     }
 
