d4182ec8279ece5b71a88ca0b198e7b2e8888102
skyfoojs <skyfoojs@gmail.com>
2025-02-17 10:54:16 +0800

    Remove TODO - Completed

--- laravel/resources/views/components/waiter/table-header.blade.php
+++ laravel/resources/views/components/waiter/table-header.blade.php
@@ -1,7 +1,6 @@
 <div class="flex w-full mt-4 items-center justify-center relative">
     <i class='bx bx-chevron-left bx-md absolute left-3 cursor-pointer' onclick="window.history.back()"></i>
 
-    <!-- TODO: Implement dynamic table number. -->
     <div class="text-lg font-semibold">{{ $trackOrder }} {{ $table }}</div>
 </div>
 
