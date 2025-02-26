514b5594bfb2eb325d647b1336595e02f5bf8aea
i4GS <admin@i4.gs>
2025-02-26 13:47:14 +0800

    Added checkout function in payment controller

--- laravel/app/Http/Controllers/PaymentController.php
+++ laravel/app/Http/Controllers/PaymentController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Payment extends Controller
 {
-    //
+    public function checkout(Request $request){
+        if (!Auth::check() || Auth::user()->role !== 'Cashier') {
+            session()->forget(['username', 'userID']);
+            Auth::logout();
+
+            return redirect()->route('login')->with('error', 'Unauthorized Access');
+        }
+
+        $tax = $subtotal * 0.06;
+        $serviceCharge = $subtotal * 0.10;
+        $total = $subtotal + $tax + $serviceCharge;
+
+        return view('cashier.payment');
+    }
 }
