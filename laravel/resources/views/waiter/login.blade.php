d3fc85c68ff8eaeebeed2f9174a7378ecf8599da
i4GS <admin@i4.gs>
2025-03-01 13:43:06 +0800

    Added mb for Topic login with

--- laravel/resources/views/waiter/login.blade.php
+++ laravel/resources/views/waiter/login.blade.php
@@ -14,7 +14,7 @@
     </div>
 
     <div class="mt-12 flex flex-col justify-center items-center">
-        <h1 class="font-bold">Login With</h1>
+        <h1 class="font-bold mb-6">Login With</h1>
             @if (session('error'))
                 <span class="w-full bg-red-200 text-center m-3 text-gray-600 p-3 rounded-lg">{{ session('error') }}</span>
             @elseif(session('logoutSuccess'))
@@ -22,8 +22,8 @@
             @endif
         <form action="{{ route('login.post') }}" method="POST" class="pt-2 flex flex-col">
             @csrf
-            <label for="email_or_username">Email or Username:</label>
-            <input required name="email_or_username" class="mt-1 border border-[#D8D8D8] rounded-md py-2 px-4 w-60 md:w-80" placeholder="Enter Email or Username" type="text">
+            <label for="email_or_username">Email/ Username</label>
+            <input required name="email_or_username" class="mt-2 border border-gray-300 rounded-lg px-3 py-2 flex-1 focus:ring-indigo-500 focus:border-indigo-500" placeholder="Enter Email or Username" type="text">
 
             <label class="mt-3" for="password">Password</label>
             <input required name="password" class="mt-1 border border-[#D8D8D8] rounded-md py-2 px-4 w-60 md:w-80" placeholder="Enter Password" type="password">
