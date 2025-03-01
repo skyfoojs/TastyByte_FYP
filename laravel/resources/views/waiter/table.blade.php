3b38a3443a38941ba321b2bd8afe2a161a552a85
skyfoojs <skyfoojs@gmail.com>
2025-03-01 21:31:09 +0800

    Modify styles

--- laravel/resources/views/waiter/table.blade.php
+++ laravel/resources/views/waiter/table.blade.php
@@ -1,19 +1,19 @@
 <x-waiter.layout>
     <x-waiter.navbar>
         <x-waiter.table-header :table="'Table Number'" :trackOrder="''">
-        <div class="flex flex-col justify-center items-center">
-            <div class="mt-20">
-                <p class="text-xl">Please Enter the Table Number</p>
-            </div>
+            <div class="absolute inset-0 flex justify-center items-center px-4">
+                <div class="flex flex-col bg-white p-6 shadow-lg rounded-lg w-full max-w-xs">
+                    <p class="text-xl font-bold">Please Enter the Table Number</p>
 
-            <form action="{{ route('storeTable') }}" method="POST">
-                @csrf
-                <input name="table" type="number" id="table" class="w-72 border border-gray-300 rounded-lg py-2 px-3 mt-10 bg-white" required>
-                <button type="submit" class="bg-indigo-500 hover:bg-indigo-600 text-white text-sm font-medium py-2 px-4 rounded-lg flex items-center space-x-2 mt-10">
-                    <span>Confirm</span>
-                </button>
-            </form>
-        </div>
+                    <form action="{{ route('storeTable') }}" method="POST">
+                        @csrf
+                        <input name="table" type="number" id="table" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 mt-6" required>
+                        <div class="flex justify-end space-x-4 mt-6">
+                            <button type="submit" class="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">Confirm</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </x-waiter.table-header>
     </x-waiter.navbar>
 </x-waiter.layout>
