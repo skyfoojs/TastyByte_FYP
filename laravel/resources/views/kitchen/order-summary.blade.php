18183105046b06f362be083e11f555f14c3cf528
i4GS <admin@i4.gs>
2025-03-02 13:09:58 +0800

    Added pending and completed categories button

--- laravel/resources/views/kitchen/order-summary.blade.php
+++ laravel/resources/views/kitchen/order-summary.blade.php
@@ -1,5 +1,13 @@
 <x-kitchen.layout>
     <x-kitchen.navbar>
+        <div class="flex h-screen relative">
+            <div class="w-3/4 p-6 mt-25">
+                <div class="flex items-center space-x-6 mt-2">
+                    <button class="p-3 bg-indigo-500 text-white font-black tracking-normal px-10 rounded-full">Pending</button>
+                    <button class="p-3 bg-indigo-200 text-indigo-500 tracking-normal px-10 rounded-full">Completed</button>
+                </div>
+            </div>
+        </div>
         <div>
             <!--TODO: Track Order Real Time, Show out with category -->
         </div>
