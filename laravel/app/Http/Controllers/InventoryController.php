5755e3f761b98bc2fe0ae8fcdbad9cb38ba84354
i4GS <admin@i4.gs>
2025-03-03 10:37:43 +0800

    Added Inventory Controller

--- laravel/app/Http/Controllers/InventoryController.php
+++ laravel/app/Http/Controllers/InventoryController.php
@@ -1,1 +1,26 @@
 <?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Inventory;
+
+use App\Models\Vouchers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
+class InventoryController extends Controller
+{
+    public function trackInventory(Request $request){
+        if (Auth::check() || Auth::user()->role == 'Kitchen'){
+            $query = Vouchers::with('product');
+
+            $inventory = $query->get();
+
+            return response()->json([
+                'inventory' => $inventory,
+            ]);
+        }
+        return redirect()->route('login')->with('error', 'Unauthorized Access');
+    }
+}
