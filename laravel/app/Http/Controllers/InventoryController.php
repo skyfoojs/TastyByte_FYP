d3051e3e08dafe737addf77501486c5855bc0b08
i4GS <admin@i4.gs>
2025-03-03 11:51:24 +0800

    Added function getFilteredInventories

--- laravel/app/Http/Controllers/InventoryController.php
+++ laravel/app/Http/Controllers/InventoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Inventory;
 
+use App\Models\Product;
 use App\Models\Vouchers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -73,5 +74,48 @@ class InventoryController extends Controller
         } else {
             return redirect()->route('admin-inventory')->with('error', 'Error updating inventory.');
         }
+    }
+
+    public function getFilteredInventories(Request $request)
+    {
+        $request->validate([
+            'filterType' => 'required|string|in:filterInventoryID,filterCategoryName,filterProductName,filterStockMoreThan,filterStockLessThan',
+            'keywords' => 'required|string',
+        ]);
+
+        $query = Inventory::query();
+
+        $filterType = $request->input('filterType');
+        $keywords = $request->input('keywords');
+
+        // Apply filtering based on filter type
+        switch ($filterType) {
+            case 'filterInventoryID':
+                $query->where('inventoryID', $keywords);
+                break;
+            case 'filterCategoryName':
+                $query->where('name', 'LIKE', "%$keywords%");
+                break;
+            case 'filterProductName':
+                $query->whereHas('product', function ($q) use ($keywords) {
+                    $q->where('name', 'LIKE', "%$keywords%");
+                });
+                break;
+            case 'filterStockMoreThan':
+                $query->where('stockLevel', '>', (int)$keywords);
+                break;
+            case 'filterStockLessThan':
+                $query->where('stockLevel', '<', (int)$keywords);
+                break;
+        }
+
+        $totalInventory = Inventory::count();
+        $limit = 6;
+        $totalPages = ceil($totalInventory / $limit);
+        $inventory = $query->paginate($limit);
+
+        $product = Product::all();
+
+        return view('admin.inventory', compact('inventory', 'totalPages', 'product'));
     }
 }
