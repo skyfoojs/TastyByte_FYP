070ef78df7acaab93937af20b54204c990541756
i4GS <admin@i4.gs>
2025-03-06 20:42:02 +0800

    Added meta tag csrf token

--- laravel/resources/views/components/kitchen/layout.blade.php
+++ laravel/resources/views/components/kitchen/layout.blade.php
@@ -3,9 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Kitchen Dashboard</title>
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
