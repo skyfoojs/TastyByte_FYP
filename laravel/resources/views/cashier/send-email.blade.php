d9aa88b47469e48b10db384a92836d766b85cd3b
skyfoojs <skyfoojs@gmail.com>
2025-03-07 01:13:28 +0800

    Add action and add hidden input

--- laravel/resources/views/cashier/send-email.blade.php
+++ laravel/resources/views/cashier/send-email.blade.php
@@ -6,12 +6,12 @@
                     <div class="p-6">
                         <h2 class="text-lg font-semibold text-gray-900 mb-6">Enter Recipient Email</h2>
 
-                        <form method="POST">
+                        <form method="POST" action="{{ route('cashier.sendEmail') }}">
                             @csrf
                             <input name="emailAddress" type="email" id="emailAddress"
                                    class="mb-6 w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
                                    placeholder="Please enter recipient email address" required>
-                            <input type="hidden" name="source" value="cashier">
+                            <input type="hidden" name="paymentID" value="{{ $paymentID }}">
                             <div class="flex justify-end space-x-4">
                                 <button type="submit" class="px-4 py-2 text-white bg-indigo-500 rounded-md hover:bg-indigo-600">Confirm Send</button>
                             </div>
