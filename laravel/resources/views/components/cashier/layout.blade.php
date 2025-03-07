6794ce481662c9d69b843858a3ce9147b0ac63d8
i4GS <admin@i4.gs>
2025-03-08 02:03:03 +0800

    Added dynamic title

--- laravel/resources/views/components/cashier/layout.blade.php
+++ laravel/resources/views/components/cashier/layout.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Cashier Order Page</title>
+        <title>@section('title') Default Title @show</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
