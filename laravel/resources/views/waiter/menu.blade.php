e70ec30691858f49d3286eeea9d4d083ad6b6782
i4GS <admin@i4.gs>
2025-03-07 15:18:12 +0800

    Replace the image as boxicons

--- laravel/resources/views/waiter/menu.blade.php
+++ laravel/resources/views/waiter/menu.blade.php
@@ -2,21 +2,21 @@
     <x-waiter.navbar>
 
     <div class="flex flex-col justify-center items-center px-2">
-        <p class="mt-6 font-semibold">Menu</p>
+        <p class="mt-6 font-semibold text-xl">Waiter Menu</p>
 
-        <div class="grid grid-cols-2 gap-x-4 gap-y-7 mt-5">
-            <a href="{{ route('table') }}" class="bg-white px-10 pt-10 pb-7 rounded-lg flex flex-col justify-center items-center">
+        <div class="grid grid-cols-2 gap-x-4 gap-y-4 mt-5 p-2">
+            <a href="{{ route('table') }}" class="bg-white p-10 rounded-xl shadow shadow-indigo-200 flex flex-col justify-center items-center">
                 <div class="p-2 rounded-lg">
-                    <img class="" src="{{ asset('images/' . 'menu-order.png') }}" alt="">
+                    <i class="bx bx-shopping-bag text-6xl text-gray-600"></i>
                 </div>
-                <p class="mt-2 text-center">Order</p>
+                <p class="mt-4 font-bold text-center text-lg text-gray-600">Order Food</p>
             </a>
 
-            <a href="{{ route('trackOrder') }}" class="bg-white px-10 pt-10 pb-7 rounded-lg flex flex-col justify-center items-center">
+            <a href="{{ route('trackOrder') }}" class="bg-white p-10 rounded-xl shadow shadow-indigo-200 flex flex-col justify-center items-center">
                 <div class="p-2 rounded-lg">
-                    <img class="" src="{{ asset('images/' . 'menu-track-order.png') }}" alt="">
+                    <i class="bx bx-time text-6xl text-gray-600"></i>
                 </div>
-                <p class="mt-2 text-center">Track Order</p>
+                <p class="mt-4 font-bold text-center text-lg text-gray-600">Track Order</p>
             </a>
         </div>
     </div>
