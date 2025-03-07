b3cd85e4372ed59f72ea8a0f32c93da05bb3f5e4
i4GS <admin@i4.gs>
2025-03-08 00:33:39 +0800

    Change word category to name

--- laravel/resources/views/admin/inventory.blade.php
+++ laravel/resources/views/admin/inventory.blade.php
@@ -24,7 +24,7 @@
                     <thead>
                     <tr class="text-gray-500 font-medium text-center">
                         <th class="py-4 px-6 border-b border-gray-200">Inventory ID</th>
-                        <th class="py-4 px-6 border-b border-gray-200">Category</th>
+                        <th class="py-4 px-6 border-b border-gray-200">Name</th>
                         <th class="py-4 px-6 border-b border-gray-200">Stock Level</th>
                         <th class="py-4 px-6 border-b border-gray-200">Minimum Level</th>
                         <th class="py-4 px-6 border-b border-gray-200">Edit</th>
