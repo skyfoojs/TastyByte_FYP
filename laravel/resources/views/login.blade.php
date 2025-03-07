f1ad45757c988e3e5e9bdd66fc4ad94ed284b437
i4GS <admin@i4.gs>
2025-03-07 14:22:42 +0800

    Removed restaurant information and center all at the middle

--- laravel/resources/views/login.blade.php
+++ laravel/resources/views/login.blade.php
@@ -1,22 +1,17 @@
 <x-layout>
-    <div class="flex flex-col p-10 xl:flex-row xl:justify-evenly bg-slate-200 items-center">
-        <div class="flex flex-col justify-center items-center">
-            <h1 class="font-semibold mt-5">Restaurant's Information</h1>
-
-            <div class="flex py-4 px-8 gap-4 bg-white shadow-xl mt-4 rounded-xl">
-                <img class="h-24 w-24 rounded-2xl" src="{{ asset('images/' . 'VTEN-Kopitiam-Logo.jpg') }}" alt="Logo.jpg">
-                <p class="text-xs text-slate-800 leading-5 text-center xl:w-64 mt-2">VTEN Kopitiam, Level 12, Menara PJH, Jalan Tun Abdul Razak, Precinct 2 Putrajaya, 62100, Selangor Malaysia</p>
+    <div class="min-h-screen flex justify-center items-center bg-slate-200">
+        <div class="mt-12 flex flex-col justify-center items-center bg-white p-18 rounded-2xl">
+            <div class="flex items-center space-x-2">
+                <img src="{{ asset('images/' . 'Logo.png') }}" alt="Logo" class="w-12 h-12">
+                <p class="text-gray-800 text-2xl font-bold">TastyByte</p>
             </div>
-        </div>
-
-        <div class="mt-12 flex flex-col justify-center items-center bg-white p-10 rounded-2xl">
-            <h1 class="font-bold mb-6">Login With</h1>
+            <hr class="w-full border-t-1 border-gray-300 mt-6">
             @if (session('error'))
                 <span class="w-full bg-red-200 text-center m-3 text-gray-600 p-3 rounded-lg">{{ session('error') }}</span>
             @elseif(session('logoutSuccess'))
                 <span class="w-full bg-red-200 text-center m-3 text-gray-600 p-3 rounded-lg">{{ session('logoutSuccess') }}</span>
             @endif
-            <form action="{{ route('login.post') }}" method="POST" class="pt-2 flex flex-col">
+            <form action="{{ route('login.post') }}" method="POST" class="pt-2 mt-6 flex flex-col">
                 @csrf
                 <label for="email_or_username">Email/ Username</label>
                 <input required name="email_or_username" class="mt-2 border border-gray-300 rounded-lg px-3 py-2 flex-1 focus:ring-indigo-500 focus:border-indigo-500" placeholder="Enter Email or Username" type="text">
@@ -24,7 +19,7 @@
                 <label class="mt-3" for="password">Password</label>
                 <input required name="password" class="mt-1 border border-[#D8D8D8] rounded-md py-2 px-4 w-60 md:w-80" placeholder="Enter Password" type="password">
 
-                <button class="py-2 px-20 bg-blue-button text-white rounded-xl mt-3 font-medium" value="submit" type="submit">LET'S GO</button>
+                <button class="py-2 px-20 bg-indigo-500 text-white rounded-full mt-6 font-bold" value="submit" type="submit">Log In</button>
             </form>
         </div>
     </div>
